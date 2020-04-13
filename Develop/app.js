d1890ddadafa8dddf38da7a133fc4ba055ce38c2
kcenters <kristincenters@gmail.com>
2020-04-12 22:08:02 -0500

    CLI prompts

--- Develop/app.js
+++ Develop/app.js
@@ -1,35 +1,41 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
+const render = require('./lib/htmlRenderer');
+
+const questions = [
+	{
+		type: 'input', name: 'name', message: 'Enter employee name',
+		validate: function validateName(name) {
+			return name !== '';
+		},
+	},
+	{
+		type: 'number', name: 'id', message: 'Enter employee ID',
+		validate: function validateID(id) {
+			return id !== '';
+		},
+	},
+	{
+		type: 'input', name: 'email',
+		message: 'Enter employee email address',
+		validate: function validateEmail(email) {
+			return email !== '';
+		},
+	},
+	{
+		type: 'checkbox',
+		message: 'Check employee role',
+		name: 'role',
+		choices: ['Engineer', 'Manager', 'Intern'],
+	},
+];
+inquirer.prompt(questions)
+	.then(function (answers) {
+		console.log(answers)
+	})
