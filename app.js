5e4b81b62d742fbf37c8cfdfe24520d0cf2f0c77
kcenters <kristincenters@gmail.com>
2020-04-13 20:35:00 -0500

    log class engineer response

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Engineer = require('./Develop/lib/Engineer');
+const Intern = require('./Develop/lib/Intern');
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
-const render = require('./lib/htmlRenderer');
+const render = require('./Develop/lib/htmlRenderer');
 
 const questions = [
 	{
@@ -28,14 +27,49 @@ const questions = [
 			return email !== '';
 		},
 	},
-	{
+
+];
+function createEngineer() {
+	inquirer.prompt(questions)
+		.then(function (answers) {
+			console.log(answers)
+			var engineer1 = new addEngineer(answers.name, answers.id, answers.email, answers.role);
+			console.log(engineer1);
+			runProgram();
+		})
+};
+
+function runProgram() {
+	inquirer.prompt({
 		type: 'checkbox',
 		message: 'Check employee role',
 		name: 'role',
-		choices: ['Engineer', 'Manager', 'Intern'],
-	},
-];
-inquirer.prompt(questions)
-	.then(function (answers) {
+		choices: ['Engineer', 'Manager', 'Intern', 'done']
+
+	}).then(function (answers) {
 		console.log(answers)
+		if (answers.role[0] === "Engineer") {
+			createEngineer();
+
+		} else if (answers.role[0] === "done") {
+			console.log("we are done");
+		}
 	})
+}
+runProgram();
+
+
+class addEngineer {
+	constructor(name, id, email, github) {
+		this.name = name;
+		this.id = id;
+		this.email = email;
+		this.github = github;
+	}
+	getEngineer() {
+		return `${this.name}, ${this.id}, ${this.email}`;
+	}
+}
+
+
+
