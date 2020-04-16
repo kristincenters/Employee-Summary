0964267a8621909da6e4b63cd72e77b51fc67ba0
kcenters <kristincenters@gmail.com>
2020-04-15 19:43:01 -0500

    created html

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
 const render = require('./Develop/lib/htmlRenderer');
 
-const questions = [
+let teamArr = [];
+
+const questionsEng = [
 	{
 		type: 'input', name: 'name', message: 'Enter employee name',
 		validate: function validateName(name) {
@@ -27,80 +29,117 @@ const questions = [
 			return email !== '';
 		},
 	},
-
+	{
+		type: 'input', name: 'github', message: "Enter github name"
+	}
 ];
+const questionsMgr = [
+	{
+		type: 'input', name: 'name', message: 'Enter employee name',
+		validate: function validateName(name) {
+			return name !== '';
+		},
+	},
+	{
+		type: 'number', name: 'id', message: 'Enter employee ID',
+		validate: function validateID(id) {
+			return id !== '';
+		},
+	},
+	{
+		type: 'input', name: 'email',
+		message: 'Enter employee email address',
+		validate: function validateEmail(email) {
+			return email !== '';
+		},
+	},
+	{
+		type: 'input', name: 'officeNumber', message: "What's the number?"
+	}
+]
+const questionsIntern = [
+	{
+		type: 'input', name: 'name', message: 'Enter employee name',
+		validate: function validateName(name) {
+			return name !== '';
+		},
+	},
+	{
+		type: 'number', name: 'id', message: 'Enter employee ID',
+		validate: function validateID(id) {
+			return id !== '';
+		},
+	},
+	{
+		type: 'input', name: 'email',
+		message: 'Enter employee email address',
+		validate: function validateEmail(email) {
+			return email !== '';
+		},
+	},
+	{
+		type: 'input', name: 'school', message: "School Name?"
+	}
+]
+
+function runProgram() {
+	inquirer.prompt({
+		type: 'list',
+		message: 'Check employee role',
+		name: 'role',
+		choices: ['Engineer', 'Manager', 'Intern', 'End Data Entry']
+
+
+	}).then(function (answers) {
+		console.log(answers)
+
+		if (answers.role === "Engineer") {
+			createEngineer();
+		}
+		else if (answers.role === "Manager") {
+			createManager();
+		}
+		else if (answers.role === "Intern") {
+			createIntern();
+		}
+		else if (answers.role === "End Data Entry") {
+			console.log("end employee data entry");
+			console.log(teamArr);
+			var holdRender = render(teamArr)
+			console.log(holdRender);
+
+		}
+	})
+}
 function createEngineer() {
-	inquirer.prompt(questions)
+	inquirer.prompt(questionsEng)
 		.then(function (answers) {
 			console.log(answers)
 			var engineer1 = new addEngineer(answers.name, answers.id, answers.email, answers.github);
-			console.log(engineer1);
+			teamArr.push(engineer1);
+			console.log(teamArr);
 			runProgram();
+
 		})
 };
 function createManager() {
-	inquirer.prompt(questions)
+	inquirer.prompt(questionsMgr)
 		.then(function (answers) {
 			console.log(answers)
-			var manager1 = new addManager(answers.name, answers.id, answers.email, answers.role);
-			console.log(manager1);
+			var manager1 = new addManager(answers.name, answers.id, answers.email, answers.officeNumber);
+			teamArr.push(manager1);
 			runProgram();
 		})
 };
 function createIntern() {
-	inquirer.prompt(questions)
+	inquirer.prompt(questionsIntern)
 		.then(function (answers) {
 			console.log(answers)
-			var intern1 = new addIntern(answers.name, answers.id, answers.email, answers.role);
-			console.log(intern1);
+			var intern1 = new addIntern(answers.roleData, answers.name, answers.id, answers.email, answers.school);
+			teamArr.push(intern1);
 			runProgram();
 		})
 };
-
-function runProgram() {
-	inquirer.prompt({
-		type: 'checkbox',
-		message: 'Check employee role',
-		name: 'role',
-		choices: ['Engineer', 'Manager', 'Intern', 'done']
-
-	}).then(function (answers) {
-		console.log(answers)
-		if (answers.role[0] === "Engineer") {
-			inquirer.prompt({
-				type: "input",
-				name: "github",
-				message: "Enter github name"
-			}).then(response => {
-				console.log(response);
-				createEngineer();
-			})
-		}
-		else if (answers.role[1] === "Manager") {
-			inquirer.prompt({
-				type: "input",
-				name: "officeNumber",
-				message: "Enter office number"
-			}).then(response => {
-				console.log(response);
-				createManager();
-			})
-		}
-		else if (answers.role[2] === "Intern") {
-			inquirer.prompt({
-				type: "input",
-				name: "school",
-				message: "Enter school name"
-			}).then(response => {
-				console.log(response);
-				createIntern();
-			})
-		}
-		else if (answers.role[3] === "done") {
-			console.log("end employee data entry");
-		}
-	})
-}
 runProgram();
 
 class addEmployee {
@@ -120,6 +159,7 @@ class addEngineer {
 		this.id = id;
 		this.email = email;
 		this.github = github;
+		this.role = "Engineer";
 	}
 	getEngineer() {
 		return `${this.name}, ${this.id}, ${this.email}, ${this.github}`;
@@ -131,6 +171,7 @@ class addManager {
 		this.id = id;
 		this.email = email;
 		this.officeNumber = officeNumber
+		this.role = "Manager";
 	}
 	getManager() {
 		return `${this.name}, ${this.id}, ${this.email}, ${this.officeNumber}`;
@@ -142,13 +183,14 @@ class addIntern {
 		this.id = id;
 		this.email = email;
 		this.school = school;
+		this.role = "Intern";
 	}
 	getIntern() {
 		return `${this.name}, ${this.id}, ${this.email}, ${this.school}`;
 	}
 }
 //render();
-/*fs.writeFile(outputPATH, output, (err)  => {
+/*fs.writeFile(outputPATH, render(teamArr), (err)  => {
 					if (err) {
 						return console.log(err);
 					}
